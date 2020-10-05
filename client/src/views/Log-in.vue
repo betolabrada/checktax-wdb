7ce82e5f191e38c6ebc0688938c97c9b40c269cc
Alan Gonzalez <argaalan@hotmail.com>
2020-10-05 17:23:16 -0600

    Basic login done

---
+++ client/src/views/Log-in.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="log-in">
+    <login></login>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Login from "@/components/Log-in.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    Login,
+  },
+})
+export default class Home extends Vue {}
+</script>
