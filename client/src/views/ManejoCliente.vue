cb22e00853f0d7ed9150858e8f9924ea41ffff44
DanielLepeVega <danylepevega@gmail.com>
2020-10-22 14:05:42 -0500

    Ventana Manejo Clientes completa
    Signed-off-by: DanielLepeVega <danylepevega@gmail.com>

---
+++ client/src/views/ManejoCliente.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="ManejoClientes">
+        <manejo-clientes></manejo-clientes>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import ManejoClientes from '@/components/ManejoClientes.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    ManejoClientes,
+  },
+})
+export default class ManejoCliente extends Vue {}
+</script>
+
