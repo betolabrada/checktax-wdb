7206b4afc6ed2495e330c4303736e4f06bf581f0
Alberto <albertolabrada99@gmail.com>
2020-10-22 18:34:45 -0600

    Integrando manejo clientes a la nav de vd
    - pequeños cambios en ui

--- client/src/components/Financiamientos.vue
+++ client/src/components/Financiamientos.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Vue } from 'vue-property-decorator';
 
 export default class Financiamiento extends Vue{}   
 </script>
