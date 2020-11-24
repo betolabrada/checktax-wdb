227d6ec299a2e87f3d690f3f1c0dd63437d38710
Alberto <albertolabrada99@gmail.com>
2020-11-24 15:38:52 -0600

    database

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
     pool: {
         user: process.env.ORCL_USER,
-        password: process.env.ORCL_PASSWORD2,
-        connectionString: process.env.ORCL_CONN2,
+        password: process.env.ORCL_PASSWORD,
+        connectionString: process.env.ORCL_CONN,
         poolMin: 10,
         poolMax: 10,
         poolIncrement: 0
