0b1b94d8722a025feb25f5d46fc109af1bda0645
Alan Gonzalez <argaalan@hotmail.com>
2020-11-25 21:26:48 -0700

    Financiamientos globales y solutos calculados

--- angular-client/src/app/services/calculations.service.ts
+++ angular-client/src/app/services/calculations.service.ts
@@ -10,24 +10,49 @@ export class CalculationsService {
   intereses: number;
   fecha: Date;
   iva: number;
-  global: boolean;
+  mensual: boolean;
 
-  constructor(capital: number, plazo: number, intereses: number, fecha: Date, iva: number, global :boolean) {
+  constructor(capital: number, plazo: number, intereses: number, fecha: Date, iva: number, mensual :boolean) {
     this.capital = capital;
     this.plazo = plazo;
     this.intereses = intereses;
     this.fecha = fecha;
     this.iva = iva;
-    this.global = global;
+    this.mensual = mensual;
   }
 
-  calcularPagoMenSaldosInsolutos(pago: number, capital: number){
+  addMonthToDate(date: Date){
+    return new Date(date.setMonth(date.getMonth()+8));
+  }
+
+  addFifteenDaysToDate(date: Date){
+    if(date.getDay()==15){
+      let result = new Date(date);
+      result.setDate(result.getDate() + 15);
+      return result;
+    } else {
+      let result = new Date(date);
+      result.setDate(result.getDate() - 15);
+      result = this.addMonthToDate(result);
+      return result;
+    }
+  }
+
+  calcularPagoMenSaldosInsolutos(pago: number, capital: number, mensual: boolean, fecha: Date){
     let actCapital = capital-pago;
     let intereses = (capital*this.intereses)/12;
     let iva = intereses*this.iva;
     let total = pago+intereses+iva;
+    let actFecha;
+
+    if(mensual){
+      actFecha = this.addMonthToDate(fecha);
+    } else {
+      actFecha = this.addFifteenDaysToDate(fecha);
+    }
 
     let pagoMen = {
+      fecha: actFecha,
       capital: actCapital,
       intereses: intereses,
       iva: iva,
@@ -40,24 +65,34 @@ export class CalculationsService {
   calcularPagosSaldosInsolutos(){
     let pagosMen = [];
     let actCapital = this.capital;
+    let actDate = this.fecha;
     const pago = this.capital/this.plazo;
 
     for(let i = 0; i < this.plazo; i++){
-      let men = this.calcularPagoMenSaldosInsolutos(pago, actCapital);
+      let men = this.calcularPagoMenSaldosInsolutos(pago, actCapital, this.mensual, actDate);
       pagosMen.push(men);
       actCapital = men.capital;
+      actDate = men.fecha;
     }
 
     return pagosMen;
   }
 
-  calcularPagoMenGlobal(pago: number, capital: number){
+  calcularPagoMenGlobal(pago: number, capital: number, mensual: boolean, fecha: Date){
     let actCapital = capital-pago;
     let intereses = (this.capital*this.intereses)/12;
     let iva = intereses*this.iva;
-    let total = pago+intereses+iva;
+    let total = pago+intereses+iva
+    let actFecha;
+
+    if(mensual){
+      actFecha = this.addMonthToDate(fecha);
+    } else {
+      actFecha = this.addFifteenDaysToDate(fecha);
+    }
 
     let pagoMen = {
+      fecha: actFecha,
       capital: actCapital,
       intereses: intereses,
       iva: iva,
@@ -70,12 +105,14 @@ export class CalculationsService {
   calcularPagosGlobal(){
     let pagosMen = [];
     let actCapital = this.capital;
+    let actDate = this.fecha;
     const pago = this.capital/this.plazo;
 
     for(let i = 0; i < this.plazo; i++){
-      let men = this.calcularPagoMenGlobal(pago, actCapital);
+      let men = this.calcularPagoMenGlobal(pago, actCapital, this.mensual, actDate);
       pagosMen.push(men);
       actCapital = men.capital;
+      actDate = men.fecha;
     }
 
     return pagosMen;
